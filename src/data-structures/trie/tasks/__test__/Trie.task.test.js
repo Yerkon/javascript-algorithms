78de820e40890166217f25d5242b1bb53b4618ba
Yerkebulan A.Y <yerkebulan_abzhapbarov@epam.com>
2018-12-21 22:33:01 +0600

    some changes

---
+++ src/data-structures/trie/tasks/__test__/Trie.task.test.js
@@ -0,0 +1,16 @@
+
+
+describe('test task', () => {
+    it('should check', () => {
+
+        let k = 3 + 2;
+
+        expect(1).toEqual(1);
+    });
+
+    // it('should test', () => {
+    //     let trie = new TrieNode('a');
+
+    //     expect(trie).toBeDefined();
+    // });
+});
